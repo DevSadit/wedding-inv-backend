43b7640864537bd1ba273cea4c543c70378766f0
DevSadit <dev.sadit@gmail.com>
2024-10-06 18:05:27 +0600

    user register fn added

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://ahsan-tumpa.netlify.app/"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irm8dks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    // await client.connect();
+
+    // ///////////////////////////////////////
+    const userCollection = client.db(`Wedding`).collection(`Users`);
+
+    // send user data to the database
+    app.post("/user", async (req, res) => {
+      const userDetails = req.body;
+      console.log(userDetails);
+      const result = await userCollection.insertOne(userDetails);
+      res.send(result);
+    });
+
+    // get user data from db
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // /////////////////////////////////////////////////
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello hi World!");
+  res.send("Hello hioooo World!");
 });
 
 app.listen(port, () => {
