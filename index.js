b571eea276353c37363cf6bde498169a687b79ac
DevSadit <dev.sadit@gmail.com>
2024-10-14 11:19:50 +0600

    user register and admin login problem solved

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ahsan-tumpa.netlify.app/"],
+    origin: ["http://localhost:5173", "https://ahsan-tumpa.netlify.app"],
     credentials: true,
   })
 );
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irm8dks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,16 +27,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server
-    // await client.connect();
+    // verify admin middleware
+    const verifyAdmin = async (req, res, next) => {
+      const user = req.user;
+      const query = { email: user?.email };
+      const result = await usersCollection.findOne(query);
+      console.log(result?.role);
+      if (!result || result?.role !== "admin")
+        return res.status(401).send({ message: "unauthorized access!!" });
 
-    // ///////////////////////////////////////
+      next();
+    };
+
     const userCollection = client.db(`Wedding`).collection(`Users`);
+    const adminCollection = client.db(`Wedding`).collection(`Admin`);
 
     // send user data to the database
     app.post("/user", async (req, res) => {
       const userDetails = req.body;
-      console.log(userDetails);
+      // console.log(userDetails);
       const result = await userCollection.insertOne(userDetails);
       res.send(result);
     });
@@ -47,6 +55,33 @@ async function run() {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    // admin
+    app.post("/adm-login", async (req, res) => {
+      const { email, password } = req.body;
+      // console.log(email, password);
+
+      try {
+        // Find user by email
+        const user = await adminCollection.findOne({ email });
+        // console.log(user);
+        if (!user) {
+          return res.status(400).json({ message: "User not found" });
+        }
+
+        // Check password
+        if (password !== user.password) {
+          // console.log(`not matching`);
+          return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // If everything is correct, send the role
+        res.send({ role: user.role });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+      }
     });
 
     // /////////////////////////////////////////////////
@@ -61,7 +96,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello hioooo World!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
